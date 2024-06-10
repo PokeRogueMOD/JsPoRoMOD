725bcf6a37eb683afc5164d85b22fd678bd106ad
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-10 05:14:23 +0200

    Basics working now.

--- inject.js
+++ inject.js
@@ -1,6 +1,15 @@
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/PokeRogueMOD/JsPoRoMOD/main/js-poro-mod/mod.min.js';
+    script.src = 'https://raw.githack.com/PokeRogueMOD/JsPoRoMOD/main/js-poro-mod/mod.min.js';
     script.type = 'text/javascript';
+    script.onload = () => {
+        console.log('PokeRogueMOD loaded.');
+        if (typeof PokeRogueMOD !== 'undefined' && PokeRogueMOD.startPopup) {
+            PokeRogueMOD.startPopup();
+        } else {
+            console.error('PokeRogueMOD.startPopup is not defined.');
+        }
+    };
+    script.onerror = () => console.error('Failed to load PokeRogueMOD.');
     document.head.appendChild(script);
 })();
