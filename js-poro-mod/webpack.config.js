3bbe2a10b1d72c54470ea54f40bb7711e6ec283d
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-10 04:36:04 +0200

    New test version.

--- js-poro-mod/webpack.config.js
+++ js-poro-mod/webpack.config.js
@@ -1,55 +1,55 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/main.js',
-  output: {
-    filename: 'mod.min.js',
-    path: path.resolve(__dirname, 'js-poro-mod'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.html$/i,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/i,
-        type: 'asset/resource',
-      },
+    mode: "production",
+    entry: "./js/main.js",
+    output: {
+        filename: "mod.min.js",
+        path: path.resolve(__dirname, "js-poro-mod"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: "raw-loader",
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                        passes: 3,
+                    },
+                    format: {
+                        comments: false,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                },
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ["mod.min.js"],
+        }),
     ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            passes: 3,
-          },
-          format: {
-            comments: false,
-          },
-          mangle: {
-            safari10: true,
-          },
-        },
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(),
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['mod.min.js'],
-    }),
-  ],
 };
