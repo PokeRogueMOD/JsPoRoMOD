0ca49ad0b4f29424e9c7fce2d353d281a5a93e8a
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-10 04:58:17 +0200

    .

--- js-poro-mod/js/main.js
+++ js-poro-mod/js/main.js
@@ -1,11 +1,14 @@
-import "../assets/styles/main.css";
-import popupHtml from "../layouts/popup.html";
-import { initPopup } from "./components/popup.js";
+import '../assets/styles/main.css';
+import popupHtml from '../layouts/popup.html';
+import { initPopup } from './components/popup.js';
 
-window.addEventListener("load", () => {
-    console.log("Loading popup HTML");
-    document.body.insertAdjacentHTML("beforeend", popupHtml);
-    console.log("Popup HTML injected");
-    initPopup();
-    console.log("initPopup called");
+window.addEventListener('load', () => {
+  console.log('Loading popup HTML');
+  document.body.insertAdjacentHTML('beforeend', popupHtml);
+  console.log('Popup HTML injected');
+  initPopup();
+  console.log('initPopup called');
 });
+
+export { initPopup };
+    
