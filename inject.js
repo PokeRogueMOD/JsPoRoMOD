2d807aec633c380f03fa1784b6475ead37875b7f
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-16 22:27:59 +0200

    Updated inject.js

--- inject.js
+++ inject.js
@@ -1,1 +1,8 @@
-(function(){var s=document.createElement("script");s.src="https://raw.githack.com/PokeRogueMOD/JsPoRoMOD/main/js-poro-mod/mod.min.js";s.type="text/javascript";document.head.appendChild(s);})();
+(function() {
+    var s = document.createElement("script");
+    s.src = "https://raw.githack.com/PokeRogueMOD/JsPoRoMOD/main/js-poro-mod/mod.min.js";
+    s.type = "text/javascript";
+    document.head.appendChild(s);
+    return "MPB MOD Overlay loaded.";
+}
+)();
