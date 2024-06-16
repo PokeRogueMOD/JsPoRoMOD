50c6dc542cdd24511921a5238baa81404acc74eb
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-16 18:50:25 +0200

    Added script to clear local cache.

---
+++ scripts/clear_local_data.js
@@ -0,0 +1,2 @@
+// This will delete your local cached data to ensure the python modded data will be loaded instead of the cached!
+Phaser.Display.Canvas.CanvasPool.pool[0].parent.game.scene.keys.battle.gameData.clearLocalData();
