59e22b49dc9b54ddde5e599cfe567bdf8edc061c
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-22 23:24:42 +0200

    Added version 1.1 tag for plugins, icons and a description.

--- Firefox/background.js
+++ Firefox/background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.includes('https://pokerogue.net/')) {
+    if (changeInfo.status === 'complete' && tab.url && tab.url.includes('https://pokerogue.net/')) {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
         files: ['injector.js']
